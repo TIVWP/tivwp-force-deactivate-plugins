c60d986db36481a04e21bd833da7385b3b113667
Gregory K <gregory@tiv.net>
2017-04-23 00:16:44 -0400

    Converting to mu-plugin

--- tivwp-force-deactivate-plugins.php
+++ tivwp-force-deactivate-plugins.php
@@ -8,8 +8,26 @@
  * Version: 1.0.1
  * License: GPL-3.0
  * License URI: http://www.gnu.org/licenses/gpl.txt
+ *
+ * @attention This file must be copied to the `mu-plugins` folder.
  */
 
-if ( is_file( dirname( __FILE__ ) . '/tivwp-force-deactivate-plugins/main.php' ) ) {
-	require_once dirname( __FILE__ ) . '/tivwp-force-deactivate-plugins/main.php';
+if (
+	! ( defined( 'WP_LOCAL_DEV' ) && WP_LOCAL_DEV )
+	&& defined( 'TIVWP_LOCAL_DEV_PLUGINS' ) && TIVWP_LOCAL_DEV_PLUGINS
+) {
+
+	/**
+	 * Hooks into the `option_active_plugins` filter and does the disabling.
+	 * @see get_option()
+	 *
+	 * @param array $plugins WP-provided list of plugins.
+	 *
+	 * @return array The filtered array of plugins.
+	 */
+	function tivwp_force_deactivate_plugins( $plugins ) {
+		return array_diff( $plugins, TIVWP_LOCAL_DEV_PLUGINS );
+	}
+
+	add_filter( 'option_active_plugins', 'tivwp_force_deactivate_plugins' );
 }
